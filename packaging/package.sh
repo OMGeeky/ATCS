ecc9a35c69a426cc9ec73fdb822689157f935003
OMGeeky <39029799+OMGeeky@users.noreply.github.com>
2024-01-01 10:47:50 -0800

    Allow it to be ran on windows
    (via Git-Bash)

--- packaging/package.sh
+++ packaging/package.sh
@@ -1,3 +1,12 @@
+#windows or linux
+if [ "$1" = "-windows" ] ; then
+    echo "Got '-windows' flag. Running Windows version"
+    LINUX=false
+else
+    echo "No '-windows' flag. Running linux version"
+    LINUX=true
+fi
+
 #read the folder this script should be in (should be the packaging folder inside the ATCS source)
 PACKAGING_DIR=$(dirname $(readlink -f "$0" || greadlink -f "$0" || stat -f "$0"))
 ATCS_SOURCE_DIR=$(dirname "${PACKAGING_DIR}")
@@ -25,13 +34,18 @@ MANIFEST_LOCATION=${TEMP_DIR}/Manifest.txt
 
 #copy lib files to packaged folder?
 echo 'copying lib files'
-mkdir ${PACKAGING_DIR}/common/lib/
+mkdir -p ${PACKAGING_DIR}/common/lib/
 cp ${ATCS_SOURCE_DIR}/lib/* ${PACKAGING_DIR}/common/lib/
 
 cd $ATCS_SOURCE_DIR
 #set ClassPath variable to use in the building etc.
 echo 'setting class path'
-CP="lib/*:src:hacked-libtiled:siphash-zackehh/src/main/java"
+#linux needs a : as seperator while windows needs ;
+if [ "$LINUX" = true ] ; then
+    CP="lib/*:src:hacked-libtiled:siphash-zackehh/src/main/java"
+else
+    CP="lib/*;src;hacked-libtiled;siphash-zackehh/src/main/java"
+fi
 echo "ClassPath: "
 echo ${CP}
 echo ""
@@ -40,6 +54,7 @@ echo ""
 echo 'building java classes'
 #javac -cp $CP *.java
 javac -cp $CP ${ATCS_SOURCE_DIR}/src/com/gpl/rpg/atcontentstudio/*.java -d ${TEMP_DIR}
+echo javac -cp $CP ${ATCS_SOURCE_DIR}/src/com/gpl/rpg/atcontentstudio/*.java -d ${TEMP_DIR}
 echo ""
 LIB_PATHS=$(find lib -name '*.jar' | paste -sd' ')
 echo "LIB_PATHS: ${LIB_PATHS}"
@@ -55,11 +70,15 @@ echo "creating jar at location: ${JAR_LOCATION}"
 # the things to add always use the whole relative path from the current dir,
 # so when that is not wanted, the -C <location> thing will change to that dir
 jar mfc ${MANIFEST_LOCATION} ${JAR_LOCATION} -C ${PACKAGING_DIR}/tmp/ com/gpl/rpg/atcontentstudio/ -C res . -C ${ATCS_SOURCE_DIR}/src .
-#-C ${ATCS_SOURCE_DIR} lib -
 
 echo ''
 echo "Done creating jar"
-cd ${PACKAGING_DIR}
-echo "Creating zip"
-tar caf "ATCS_${VERSION}.zip" "common"
-echo "Created zip at ${PACKAGING_DIR}/ATCS_${VERSION}.zip"
+
+if [ "$LINUX" = true ] ; then
+    cd ${PACKAGING_DIR}
+    echo "Creating archive"
+    tar caf "ATCS_${VERSION}.tar.gz" "common"
+    echo "Created archive at ${PACKAGING_DIR}/ATCS_${VERSION}.tar.gz"
+else
+    echo "Can't create zip files on windows yet. Please pack the content of the '${PACKAGING_DIR}/common/' folder yourself
+fi 
