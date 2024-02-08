4d03c15e089a6e0ee7c1d1d45cc6838bc9c28e08
OMGeeky <39029799+OMGeeky@users.noreply.github.com>
2024-02-08 18:38:11 +0000

    Improve some error messages

--- src/com/gpl/rpg/atcontentstudio/model/gamedata/ActorCondition.java
+++ src/com/gpl/rpg/atcontentstudio/model/gamedata/ActorCondition.java
@@ -236,7 +236,12 @@ public class ActorCondition extends JSONElement {
 		if (this.icon_id != null) {
 			String spritesheetId = this.icon_id.split(":")[0];
 			if (getProject().getSpritesheet(spritesheetId) == null) {
+				System.out.println("Actor Condition");
 				System.out.println(this.id);
+				System.out.println("failed to load spritesheet");
+				System.out.println(spritesheetId);
+				System.out.println("while creating backling for icon_id");
+				System.out.println(this.icon_id);
 			}
 			getProject().getSpritesheet(spritesheetId).addBacklink(this);
 		}
