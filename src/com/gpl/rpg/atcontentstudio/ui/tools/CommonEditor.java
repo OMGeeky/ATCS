975d13f36f45411648d4cf2ed3d0f4dda56ba59b
OMGeeky <>
2025-06-16 08:31:22 +0200

    extract wordWrap function to common class

---
+++ src/com/gpl/rpg/atcontentstudio/ui/tools/CommonEditor.java
@@ -0,0 +1,21 @@
+package com.gpl.rpg.atcontentstudio.ui.tools;
+
+public final class CommonEditor {
+
+    public static String wordWrap(String in, int length) {
+        if (in == null) return null;
+        final String newline = "\n";
+        //:: Trim
+        while(!in.isEmpty() && (in.charAt(0) == '\t' || in.charAt(0) == ' ')) in = in.substring(1);
+        //:: If Small Enough Already, Return Original
+        if(in.length() < length) return in;
+        //:: If Next length Contains Newline, Split There
+        if(in.substring(0, length).contains(newline)) return in.substring(0, in.indexOf(newline)).trim() + newline + wordWrap(in.substring(in.indexOf("\n") + 1), length);
+        //:: Otherwise, Split Along Nearest Previous Space/Tab/Dash
+        int spaceIndex = Math.max(Math.max( in.lastIndexOf(" ", length), in.lastIndexOf("\t", length)), in.lastIndexOf("-", length));
+        //:: If No Nearest Space, Split At length
+        if(spaceIndex == -1) spaceIndex = length;
+        //:: Split
+        return in.substring(0, spaceIndex).trim() + newline + wordWrap(in.substring(spaceIndex), length);
+    }
+}
